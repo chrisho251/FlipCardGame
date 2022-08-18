dd1fe83a675d3e6b1395d0e91ae68d047b6b8b46
chrisho251 <hoduyhoang250196@gmail.com>
2022-08-18 16:57:24 +0700

    Initial Commit

---
+++ FlipCardGame/FlipCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipCardGameApp.swift
+//  FlipCardGame
+//
+//  Created by Hoang, Ho Duy on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlipCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
