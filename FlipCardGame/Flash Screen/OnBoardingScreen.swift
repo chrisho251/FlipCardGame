6d72f68706842358d5fba2e2f7c7d336fa7c6c1d
chrisho251 <hoduyhoang250196@gmail.com>
2022-08-18 20:51:55 +0700

    update 18/8

--- FlipCardGame/Flash Screen/OnBoardingScreen.swift
+++ FlipCardGame/Flash Screen/OnBoardingScreen.swift
@@ -10,37 +10,42 @@ import SwiftUI
 struct OnBoardingScreen: View {
     
     var body: some View {
-        ZStack {
-            
-            Color.black.edgesIgnoringSafeArea(.all)
-            
-            PokemonView().offset(x: 0, y: -95)
-            
-            VStack {
-                
+        
+        NavigationView {
+
+            ZStack {
+                Color.black.edgesIgnoringSafeArea(.all)
                 ZStack {
+                    PokemonView().offset(x: 0, y: -95)
+                    
                     RoundedRectangle(cornerRadius: 75)
                         .frame(width: 450, height: 400, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                         .foregroundColor(.gray).opacity(0.2)
                         .offset(x: 0, y: 350)
-                    
+                        
                     VStack {
                         Text("GET STARTED")
                             .font(.title)
                             .fontWeight(.thin)
                             .foregroundColor(Color.white)
-                        ZStack {
-                            Circle()
-                                .frame(width: 75, height: 75, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                .foregroundColor(.clear)
-                                .overlay(Circle().stroke(Color.white, lineWidth: 2))
-                            Image(systemName: "arrow.right")
-                                .resizable()
-                                .frame(width: 35, height: 30, alignment: .center)
-                                .foregroundColor(.white)
+                            
+                            NavigationLink (
+                                destination: HomeScreen(),
+                                label: {
+                                    ZStack {
+                                        Circle()
+                                            .frame(width: 75, height: 75, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                            .foregroundColor(.clear)
+                                            .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                                        Image(systemName: "arrow.right")
+                                            .resizable()
+                                            .frame(width: 35, height: 30, alignment: .center)
+                                            .foregroundColor(.white)
+                                    }
+                                }
+                            )
                         }
-                    }
-                    .offset(x: 0, y: 300)
+                        .offset(x: 0, y: 300)
                 }
             }
         }
@@ -60,7 +65,8 @@ struct PokemonView: View {
     @State var shockOne = false
     @State var shockTwo = false
     @State var visibilityOpacity = false
-    
+    @State var isAnimating = false
+
     var body: some View {
         
         ZStack{
@@ -80,8 +86,9 @@ struct PokemonView: View {
                     .rotationEffect(.degrees(9))
                     .offset(x: 80, y: 0)
                     .rotationEffect(.degrees(tail ? 0 : 35))
-                    .animation(Animation.easeInOut(duration: 1).delay(5).repeatForever(autoreverses: true))
+                    .animation(Animation.easeInOut(duration: 1).delay(5).repeatForever(autoreverses: true),value: tail)
                     .onAppear(){
+                        
                             self.tail.toggle()
                         }
                 
@@ -95,7 +102,7 @@ struct PokemonView: View {
                     .frame(width: 350, height: 350, alignment: .center)
                     .shadow(color: .yellow, radius: 0.5, x: 0.0, y: 0.0)
                     .opacity(visibilityOpacity ? 1 : 0)
-                    .animation(Animation.easeInOut(duration: 0.2).delay(2).repeatForever(autoreverses: true))
+                    .animation(Animation.easeInOut(duration: 0.2).delay(2).repeatForever(autoreverses: true), value: visibilityOpacity)
                     .onAppear(){
                             self.visibilityOpacity.toggle()
                         }
@@ -107,8 +114,8 @@ struct PokemonView: View {
                     .opacity(shockOne ? 1 : 0)
                     .clipShape(Circle().offset(x: shockOne ? 0 : 125))
                     .animation(Animation.interpolatingSpring(
-                                stiffness: 30, damping: 10).speed(5).repeatForever(autoreverses: true).delay(1))
-                     .onAppear() {
+                                stiffness: 30, damping: 10).speed(5).repeatForever(autoreverses: true).delay(1), value: shockOne)
+                    .onAppear() {
                         self.shockOne.toggle()
                       }
                 
@@ -119,12 +126,11 @@ struct PokemonView: View {
                     .opacity(shockTwo ? 1 : 0)
                     .clipShape(Circle().offset(x: shockTwo ? 0 : -125))
                     .animation(Animation.interpolatingSpring(
-                            stiffness: 30, damping: 10).speed(5).repeatForever(autoreverses: true).delay(1))
-                     .onAppear() {
+                            stiffness: 30, damping: 10).speed(5).repeatForever(autoreverses: true).delay(1), value: shockTwo)
+                    .onAppear() {
                         self.shockTwo.toggle()
                       }
             }
-   
         }
     }
 }
